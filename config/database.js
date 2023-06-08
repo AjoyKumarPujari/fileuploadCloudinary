b04182590122731d65362b885a5fbe5338fcd97b
Ajoy Kumar Pujari <ajoykumarpujari@gmail.com>
2023-06-08 23:26:11 +0530

    Initial commit for database setup

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+exports.connect = () => {
+    mongoose.connect(process.env.MONGODB_URL, {
+        userNewUrlParser : true,
+        useUnifiedTopology: true,
+    })
+    .then(console.log("DB COnnected Successfully"))
+    .catch(( error) => {
+        console.log("DB Connection Issue");
+        console.error(error);
+        process.exit(1);
+    });
+};
