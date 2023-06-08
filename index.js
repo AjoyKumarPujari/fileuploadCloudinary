ea35712e2a493c6cec4fbebd59364bf0d9e7a600
Ajoy Kumar Pujari <ajoykumarpujari@gmail.com>
2023-06-09 00:30:41 +0530

    Routes are added

---
+++ index.js
@@ -0,0 +1,28 @@
+//app create
+const express = require("express");
+const app = express();
+
+//PORT findout
+const PORT = process.env.PORT || 3000;
+
+//Add middleware
+app.use(express.json());
+const fileUpload = require("express-fileupload");
+app.use(fileUpload());
+
+//connect   to DB
+const db = require("./config/database");
+db.connect();
+
+//connect to cloud
+const cloudinary = require("./config/cloudinary");
+cloudinary.cloudinaryConnect();
+
+//mount api route
+const Upload = require("./routes/FileUpload");
+app.use('api/v1/upload', Upload);
+
+//Activate Server
+app.listen(PORT, () => {
+    console.log(`App is running at port ${PORT}`);
+})
