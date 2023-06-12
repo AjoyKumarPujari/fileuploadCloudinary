1cafff99fcf8051b825831325a8552cb9c15e6b7
Ajoy Kumar Pujari <ajoykumarpujari@gmail.com>
2023-06-13 00:39:48 +0530

    mail send via node mailer added

--- models/File.js
+++ models/File.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 
 const fileSchema = new mongoose.Schema({
     name:{
@@ -15,6 +16,38 @@ const fileSchema = new mongoose.Schema({
         type:String,
     }
 });
+
+//post middleware
+fileSchema.post("save", async function(doc){ //entry which are create in db are refered as doc
+    try {
+        console.log("DOC", doc);
+        //transporter
+        let transporter = nodemailer.createTransport({
+            host: process.env.MAIL_HOST,
+            auth:{
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
+            },
+
+        });
+
+
+        //send mail
+        let info = await transporter.sendMail({
+            from:"Study Notion",
+            to:doc.email,
+            subject:"New File Uploaded",
+            html:`<h2>Hello Dear</h2> <p>File Uploaded View here:<a href="${doc.imageUrl}">${doc.name}</a> </p>`,
+        })
+        console.log(info);
+
+
+    } catch (error) {
+        console.error(error);
+    }
+})
+
+
 
 const File = mongoose.model("File", fileSchema);
 module.exports = File;
